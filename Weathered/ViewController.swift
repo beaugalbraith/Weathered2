7b3dece345b8ad6acb331e27b9a744f260339112
Beau <beaugalbraith@gmail.com>
2017-07-06 13:47:11 -0500

    Initial Commit

---
+++ Weathered/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Weathered
+//
+//  Created by gem on 7/6/17.
+//  Copyright © 2017 beau. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
